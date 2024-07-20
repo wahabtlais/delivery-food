a4971bdfffab3413542f4d0d735ce5407ff39ea7
wahabtlais <hussientlais20@gmail.com>
2024-07-20 19:13:03 +0300

    project initialization

---
+++ app/constants/theme.js
@@ -0,0 +1,22 @@
+export const shadows = {
+	small: {
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 2,
+	},
+	medium: {
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 5.84,
+		elevation: 5,
+	},
+};
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "#30b9b2",
+					100: "#00fff53c",
+				},
+				secondary: {
+					DEFAULT: "#ffa44f",
+					100: "#ffe5db",
+				},
+				gray: {
+					DEFAULT: "#83829A",
+					100: "#C1C0C8",
+				},
+				white: {
+					DEFAULT: "#FFFFFF",
+					100: "#FAFAFC",
+					200: "#F3F4F8",
+				},
+				black: {
+					DEFAULT: "#000",
+					100: "#1E1E2D",
+					200: "#232533",
+				},
+				red: "#e81e4d",
+				green: " #00C135",
+			},
+			fontFamily: {
+				pthin: ["Poppins-Thin", "sans-serif"],
+				plight: ["Poppins-Light", "sans-serif"],
+				pregular: ["Poppins-Regular", "sans-serif"],
+				pmedium: ["Poppins-Medium", "sans-serif"],
+				psemibold: ["Poppins-SemiBold", "sans-serif"],
+				pbold: ["Poppins-Bold", "sans-serif"],
+				pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
